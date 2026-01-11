85171fefdc125bc375b19f36a302748eb1f5f0b7
aeasx <3089326386@qq.com>
2026-01-11 21:29:39 +0800

    feat: reactive demo

--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const arr = ref(Array.from({ length: 5 }, (_, i) => i + 1))
+function handleObserver() {
+  arr.value = arr.value.map(item => item.valueOf() * 2 + 1)
+}
+</script>
+
 <template>
   <h1>AboutView</h1>
+  <div>
+    <ul>
+      <li v-for="value in arr" :key="value">
+        {{ value }}
+      </li>
+    </ul>
+    <el-button @click="handleObserver">
+      Observer
+    </el-button>
+  </div>
 </template>
